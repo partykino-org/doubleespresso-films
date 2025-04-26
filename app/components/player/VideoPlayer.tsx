9d9e6b0b89c336684058cf8e450579e6aab37a1f
Vladyslav Dmytrenko <v.dmitrenko1998@gmail.com>
2025-04-26 14:33:25 +0300

    removed custom player nav for mobile devices till 980px

--- app/components/player/VideoPlayer.tsx
+++ app/components/player/VideoPlayer.tsx
@@ -18,6 +18,7 @@ export default function VideoPlayer({ film_url }: { film_url: string }) {
   const [playbackRate, setPlaybackRate] = useState<number>(1);
   const [previousVolume, setPreviousVolume] = useState<number>(0.5);
   const [isMuted, setIsMuted] = useState<boolean>(false);
+  const [isMobile, setIsMobile] = useState<boolean>(false);
 
   const formatTime = (time: number): string => {
     let seconds = Math.floor(time % 60);
@@ -137,6 +138,13 @@ export default function VideoPlayer({ film_url }: { film_url: string }) {
     };
   }, []);
 
+  useEffect(() => {
+    const checkScreenSize = () => setIsMobile(window.innerWidth <= 980);
+    checkScreenSize();
+    window.addEventListener("resize", checkScreenSize);
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
+
   return (
     <div
       ref={containerRef}
@@ -151,99 +159,103 @@ export default function VideoPlayer({ film_url }: { film_url: string }) {
         className="w-full cursor-pointer"
         src={film_url}
       />
-      <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black via-transparent to-transparent p-4">
-        <div
-          className="h-1 bg-white/50 cursor-pointer relative"
-          onClick={(e) => {
-            const rect = (e.target as HTMLElement).getBoundingClientRect();
-            const clickX = e.clientX - rect.left;
-            if (videoRef.current) {
-              const newTime = (clickX / rect.width) * videoRef.current.duration;
-              videoRef.current.currentTime = newTime;
-            }
-          }}
-        >
+      {!isMobile ? (
+        <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black via-transparent to-transparent p-4">
           <div
-            ref={progressBarRef}
-            className="h-1 bg-blue-500 w-0 absolute top-0 left-0"
-          />
-        </div>
-        <div className="relative flex justify-between items-center mt-2">
-          <div className="flex items-center space-x-2 md:w-[250px]">
-            {isMuted ? (
-              <SpeakerIconMuted
-                onClick={() => {
-                  const video = videoRef.current;
-                  if (video) {
-                    video.volume = previousVolume;
-                    if (volumeSliderRef.current)
-                      volumeSliderRef.current.value = previousVolume.toString();
-                    setIsMuted(false);
-                  }
-                }}
-                className="text-white cursor-pointer w-4 h-4 md:w-6 md:h-6"
-              />
-            ) : (
-              <SpeakerIcon
-                onClick={() => {
-                  const video = videoRef.current;
-                  if (video) {
-                    setPreviousVolume(video.volume);
-                    video.volume = 0;
-                    if (volumeSliderRef.current)
-                      volumeSliderRef.current.value = "0";
-                    setIsMuted(true);
-                  }
-                }}
-                className="text-white cursor-pointer w-4 h-4 md:w-6 md:h-6"
-              />
-            )}
-            <input
-              ref={volumeSliderRef}
-              type="range"
-              min="0"
-              max="1"
-              step="any"
-              onChange={handleVolumeChange}
-              defaultValue="0.5"
-              className="accent-blue-500 w-[30px] md:w-[70px]"
+            className="h-1 bg-white/50 cursor-pointer relative"
+            onClick={(e) => {
+              const rect = (e.target as HTMLElement).getBoundingClientRect();
+              const clickX = e.clientX - rect.left;
+              if (videoRef.current) {
+                const newTime =
+                  (clickX / rect.width) * videoRef.current.duration;
+                videoRef.current.currentTime = newTime;
+              }
+            }}
+          >
+            <div
+              ref={progressBarRef}
+              className="h-1 bg-blue-500 w-0 absolute top-0 left-0"
             />
-            <div className="text-white text-[8px] md:text-sm">
-              {currentTime} / {duration}
+          </div>
+          <div className="relative flex justify-between items-center mt-2">
+            <div className="flex items-center space-x-2 md:w-[250px]">
+              {isMuted ? (
+                <SpeakerIconMuted
+                  onClick={() => {
+                    const video = videoRef.current;
+                    if (video) {
+                      video.volume = previousVolume;
+                      if (volumeSliderRef.current)
+                        volumeSliderRef.current.value =
+                          previousVolume.toString();
+                      setIsMuted(false);
+                    }
+                  }}
+                  className="text-white cursor-pointer w-4 h-4 md:w-6 md:h-6"
+                />
+              ) : (
+                <SpeakerIcon
+                  onClick={() => {
+                    const video = videoRef.current;
+                    if (video) {
+                      setPreviousVolume(video.volume);
+                      video.volume = 0;
+                      if (volumeSliderRef.current)
+                        volumeSliderRef.current.value = "0";
+                      setIsMuted(true);
+                    }
+                  }}
+                  className="text-white cursor-pointer w-4 h-4 md:w-6 md:h-6"
+                />
+              )}
+              <input
+                ref={volumeSliderRef}
+                type="range"
+                min="0"
+                max="1"
+                step="any"
+                onChange={handleVolumeChange}
+                defaultValue="0.5"
+                className="accent-blue-500 w-[30px] md:w-[70px]"
+              />
+              <div className="text-white text-[8px] md:text-sm">
+                {currentTime} / {duration}
+              </div>
+            </div>
+            <div className="flex gap-4">
+              <button onClick={() => skipTime(-10)} className="text-white">
+                <BackwardIcon className="w-4 h-4 md:w-6 md:h-6" />
+              </button>
+              <button onClick={togglePlayPause} className="text-white">
+                {isPlaying ? (
+                  <PlayIcon className="w-4 h-4 md:w-6 md:h-6" />
+                ) : (
+                  <PauseIcon className="w-4 h-4 md:w-6 md:h-6" />
+                )}
+              </button>
+              <button onClick={() => skipTime(10)} className="text-white">
+                <ForwardIcon className="w-4 h-4 md:w-6 md:h-6" />
+              </button>
+            </div>
+            <div className="flex items-center justify-end space-x-2 md:w-[250px]">
+              <select
+                value={playbackRate}
+                onChange={handlePlaybackRateChange}
+                className="text-white bg-black border border-white rounded text-[10px] md:text-lg"
+              >
+                <option value={0.5}>0.5x</option>
+                <option value={1}>1x</option>
+                <option value={1.5}>1.5x</option>
+                <option value={2}>2x</option>
+              </select>
+              <button onClick={toggleFullscreen} className="text-white">
+                <FullScreenIcon className="w-4 h-4 md:w-6 md:h-6" />
+              </button>
             </div>
           </div>
-          <div className="flex gap-4">
-            <button onClick={() => skipTime(-10)} className="text-white">
-              <BackwardIcon className="w-4 h-4 md:w-6 md:h-6" />
-            </button>
-            <button onClick={togglePlayPause} className="text-white">
-              {isPlaying ? (
-                <PlayIcon className="w-4 h-4 md:w-6 md:h-6" />
-              ) : (
-                <PauseIcon className="w-4 h-4 md:w-6 md:h-6" />
-              )}
-            </button>
-            <button onClick={() => skipTime(10)} className="text-white">
-              <ForwardIcon className="w-4 h-4 md:w-6 md:h-6" />
-            </button>
-          </div>
-          <div className="flex items-center justify-end space-x-2 md:w-[250px]">
-            <select
-              value={playbackRate}
-              onChange={handlePlaybackRateChange}
-              className="text-white bg-black border border-white rounded text-[10px] md:text-lg"
-            >
-              <option value={0.5}>0.5x</option>
-              <option value={1}>1x</option>
-              <option value={1.5}>1.5x</option>
-              <option value={2}>2x</option>
-            </select>
-            <button onClick={toggleFullscreen} className="text-white">
-              <FullScreenIcon className="w-4 h-4 md:w-6 md:h-6" />
-            </button>
-          </div>
         </div>
-      </div>
+      ) : null}
     </div>
   );
 }
